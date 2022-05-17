fcd796bef870bab5352e012bf6f3c9b423c42ba4
simiongitau <simiongitau33@gmail.com>
2022-05-17 18:17:59 +0300

    pick up

---
+++ client/agrovet/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './component/Navbar/Nav';
+function App() {
+  return (
+    <div className="">
+     <h1>
+       <Nav/>
+     </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/agrovet/src/component/Navbar/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>
+      <h1>welcome to navbar</h1>
+    </div>
+  )
+}
